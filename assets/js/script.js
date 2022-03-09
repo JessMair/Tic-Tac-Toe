440943d6292f5c8ce72f695044cf906376348eb8
JessMair <jessiegrewal11@gmail.com>
2022-03-09 00:27:39 +0000

    Add HTML elements, Game related, event handlers and event listners to JS file

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+// HTML Elements 
+const playerDiv = document.querySelector('.player');
+const reset-buttonDiv = document.querySelector('.reset-button');
+const cellDivs = document.querySelector('.cell')
+
+console.log(cellDiv);
+
+
+
+// Game 
+
+let gameStarted = true;
+let xIsNext = true;
+
+
+
+
+//Event Handlers
+Const handleReset = (addEventListener) parameter {
+    console.log(addEventListener)
+}
+
+Const handleCell = (addEventListener)
+
+
+//Event Listeners 
+
+resetDiv.addEventListner('click', handleReset);
+
+for (const cellDiv of cellDiv) {
+    cellDiv.addEventListner('click' handleCell)
+}
