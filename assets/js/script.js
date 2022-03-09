8d8ccd37037ef4f2fbad8e0575912c10c0942de7
JessMair <jessiegrewal11@gmail.com>
2022-03-09 00:47:25 +0000

    Add functions to the grid under event handlers to make the game responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,10 +17,27 @@ let xIsNext = true;
 
 //Event Handlers
 Const handleReset = (addEventListener) parameter {
-    console.log(addEventListener)
+    console.log(addEventListener);
 }
 
-Const handleCell = (addEventListener)
+Const handleCell = (addEventListener) parameter {
+    const classList = e.target.classList;
+    const location = addEventListener.classList[1];
+
+    if (classList[2] === 'x' || classList[2] === 'o') {
+        return;   
+    }
+
+    if (xIsNext) {
+        else.classList.add('x');
+        xIsNext = !xIsNext;
+    }   else {
+        else.classList.add('o');
+        xIsNext = !xIsNext;
+    }
+   
+  
+};
 
 
 //Event Listeners 
